9ea003225dc11660ed9dd94d1b3e4e791d57c954
Tomlifee <tomekbag11@gmail.com>
2025-12-07 18:35:00 +0100

    Transfer
    Przenoszenie na 2 laptopa

---
+++ Cprograms/UNI_LAB_8/Srlen/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define Length 100
+
+int my_srlen(const char[Length]);
+
+int main()
+{
+    //Variables
+    char buffer[Length];
+ 
+    //User input
+    fgets(buffer, Length, stdin);
+
+    //Output
+    printf("Twoj tekst ma %d znakow", my_srlen(buffer));
+
+    return 0;
+}
+
+int my_srlen(const char w[Length])
+{
+    int i = 0;
+
+    //Counting words
+    for(; i < Length; i++)
+    {
+        if(w[i] == '\0') break;
+    }
+
+    //The '\0' character fixup
+    i--;
+
+    return i;
+}
